e2ab0fa0170fc41cb42c702f3e35ab91e925aaff
Simon Roy <sroy@dept-info.crosemont.quebec>
2021-06-09 04:23:59 -0400

    Bot helpers (and color helper) --- NONE

--- bot/help.py
+++ bot/help.py
@@ -1,11 +1,14 @@
 from discord import Color, Embed
 from discord.ext import commands
 
+from bot import CONFIG
+from bot.helpers.color_helper import get_color_digit
+
 
 class Help(commands.MinimalHelpCommand):
     async def send_pages(self, ctx=None):
         help_message = Embed(
-            color=Color(0xfffffe),
+            color=get_color_digit(CONFIG.style.embed_color),
             title="Help",
             description=''
         )
@@ -19,7 +22,7 @@ class Help(commands.MinimalHelpCommand):
 
     async def send_command_help(self, command):
         help_message = Embed(
-            color=Color(0xfffffe),
+            color=get_color_digit(CONFIG.style.embed_color),
             title=f"Help for {self.clean_prefix}{command.name}",
             description=command.help
         )
