3cb4fb2df379680bf6636849a92a0114dbf3ea16
Simon Roy <sroy@dept-info.crosemont.quebec>
2021-06-09 00:28:18 -0400

    Help handling refactorized

--- bot/help.py
+++ bot/help.py
@@ -3,19 +3,29 @@ from discord.ext import commands
 
 
 class Help(commands.MinimalHelpCommand):
-    async def send_pages(self):
-        help_message = Embed(color=Color.blurple(), description='')
-        destination = self.get_destination()
+    async def send_pages(self, ctx=None):
+        help_message = Embed(
+            color=Color(0xfffffe),
+            title="Help",
+            description=''
+        )
+
+        destination = ctx.channel if ctx else self.get_destination()
 
         for page in self.paginator.pages:
             help_message.description += page
 
         await destination.send(embed=help_message)
 
-    async def send_command_help(self, ctx, command):
-        help_message = Embed(color=Color.blurple(), description='')
-        destination = ctx.channel
+    async def send_command_help(self, command):
+        help_message = Embed(
+            color=Color(0xfffffe),
+            title=f"Help for {self.clean_prefix}{command.name}",
+            description=command.help
+        )
 
-        help_message.description += f"{ctx.prefix}{command.name} {command.usage}\n\n{command.help}"
+        help_message.add_field(name="Usage", value=command.usage)
 
-        await destination.send(embed=help_message)
+        help_message.set_footer(text=f"For more help enter {self.clean_prefix}help")
+
+        await self.get_destination().send(embed=help_message)
