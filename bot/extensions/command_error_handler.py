b82b5423acb8db6c426217c9caaab1646235ed9f
Simon Roy <simon.roy1211@gmail.com>
2021-06-14 22:42:27 -0400

    Replace help by PrettyHelp to facilitate and render the help more nice for the eyes

--- bot/extensions/command_error_handler.py
+++ bot/extensions/command_error_handler.py
@@ -10,17 +10,22 @@ class CommandError(Cog):
     async def on_command_error(self, ctx, command_error):
         warning(f"Error: {command_error}. Issued by {ctx.author}")
 
-        if isinstance(command_error, MissingRequiredArgument):
-            await self.bot.help_command.send_command_help(ctx.command)
-        elif isinstance(command_error, CommandNotFound):
-            await self.bot.help_command.send_pages(ctx)
+        if isinstance(command_error, CommandNotFound):
+            await self.send_command_help(ctx)
         elif isinstance(command_error, MissingPermissions):
             await ctx.send("You don't have the authorization to use that command.")
-        elif ctx.command:
-            await self.bot.help_command.send_command_help(ctx.command)
+        elif isinstance(command_error, MissingRequiredArgument) or ctx.command:
+            await self.send_command_help(ctx)
         else:
             await ctx.send("An error occurred. Contact the the administrators")
 
+    @staticmethod
+    def send_command_help(ctx):
+        if ctx.command:
+            ctx.send_help(ctx.command)
+
+        return ctx.send_help()
+
 
 def setup(bot):
-    bot.add_cog(CommandError(bot))
+    bot.add_cog(ErrorHandler(bot))
