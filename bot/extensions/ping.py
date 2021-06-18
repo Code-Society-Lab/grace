8d9be0d930efd181598535de33a511cccb104841
amey patil <ameywpatil@gmail.com>
2021-06-18 11:03:23 +0530

    changed the usage and help

--- bot/extensions/ping.py
+++ bot/extensions/ping.py
@@ -5,13 +5,12 @@ class Ping(Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @command(name='ping', help='Send pong', usage='pong')
+    @command(name='ping', help='shows the bot latency', usage='ping')
     async def ping_command(self, ctx):
         embed = Embed(
             color=self.bot.default_color,
             description=f"pong :ping_pong:  {round(self.bot.latency * 1000)}ms",
         )
-
         await ctx.send(embed=embed);
 
 def setup(bot):
