32ae6ef023cb211cb76a223be903270ac5833516
Simon Roy <simon.roy1211@gmail.com>
2021-08-16 03:27:00 -0400

    New extension function in util to get ext from name

--- bot/utils/extensions.py
+++ bot/utils/extensions.py
@@ -23,3 +23,10 @@ def get_extensions_config():
 
             if os.path.exists(config_path):
                 yield config_path
+
+
+def get_extension(extension_name):
+    for module in pkgutil.walk_packages(extensions.__path__, f"{extensions.__name__}."):
+        if not module.ispkg and module.name.split(".")[-1] == extension_name:
+            return module.name
+
