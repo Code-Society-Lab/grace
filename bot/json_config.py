b5ff69f2759ed01081b8c4ed96a515807648048f
Simon Roy <simon.roy1211@gmail.com>
2021-08-16 00:38:06 -0400

    Temporary json config --- NONE

--- bot/json_config.py
+++ bot/json_config.py
@@ -1,7 +1,7 @@
 import json
 
 
-class Config:
+class JsonConfig:
     def __init__(self, json_config):
         self.config = json_config
         self.extensions = None
@@ -9,7 +9,7 @@ class Config:
         for key in json_config.keys():
             value = json_config.get(key)
 
-            setattr(self, key, Config(value) if isinstance(value, dict) else value)
+            setattr(self, key, JsonConfig(value) if isinstance(value, dict) else value)
 
     def load_extensions_configs(self, extensions_configs):
         json_extensions = {}
@@ -18,9 +18,9 @@ class Config:
             with open(extension_configs) as extensions_config_file:
                 json_extensions.update(json.load(extensions_config_file))
 
-        self.extensions = Config(json_extensions)
+        self.extensions = JsonConfig(json_extensions)
 
     @staticmethod
     def load_file(file):
         with open(file) as config_file:
-            return Config(json.load(config_file))
+            return JsonConfig(json.load(config_file))
