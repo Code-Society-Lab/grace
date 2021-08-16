0af297d0a4916fca1cd32c49359d57417547b23c
Simon Roy <simon.roy1211@gmail.com>
2021-08-16 03:27:19 -0400

    Extension model

---
+++ bot/models/extension.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Integer, Column, String
+from bot import app
+from bot.classes.state import State
+from bot.utils.extensions import get_extension
+from db.Model import Model
+
+
+class Extension(app.base, Model):
+    """Extension model (With SQLAlchemy ORM)"""
+    __tablename__ = "extensions"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    _state = Column("state", Integer, default=1)
+
+    @property
+    def state(self):
+        return State(self._state)
+
+    @state.setter
+    def state(self, new_state):
+        self._state = new_state
+
+    @property
+    def module(self):
+        return get_extension(self.name)
+
+    def __str__(self):
+        return f"{self.id} | {self.module} - {self.state}"
