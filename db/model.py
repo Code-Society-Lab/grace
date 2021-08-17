9eafba7f2bc383ff4aedbe3fd50f0ca62390f4e7
Simon Roy <simon.roy1211@gmail.com>
2021-08-16 21:41:06 -0400

    Added other  simplified methods to base models

--- db/model.py
+++ db/model.py
@@ -3,8 +3,20 @@ from bot import app
 
 class Model:
     @classmethod
+    def query(cls):
+        return app.session.query(cls)
+
+    @classmethod
     def get(cls, model_id):
         return app.session.query(cls).get(model_id)
+
+    @classmethod
+    def all(cls):
+        return app.session.query(cls).all()
+
+    @classmethod
+    def where(cls, *args, **kwargs):
+        return app.session.query(cls).filter_by(**kwargs)
 
     def save(self, commit=True):
         app.session.add(self)
