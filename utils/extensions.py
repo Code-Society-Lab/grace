4097e4999a4943234c9c19a1b9c9fc1eabad7857
Simon Roy <simon.roy1211@gmail.com>
2021-08-24 14:26:31 -0400

    Moved utils into the root directory (feels more right for some reason). Removed get_extension_config (deprecated)

--- utils/extensions.py
+++ utils/extensions.py
@@ -16,15 +16,6 @@ def get_extensions():
         yield module.name
 
 
-def get_extensions_config():
-    for module in pkgutil.walk_packages(extensions.__path__, f"{extensions.__name__}."):
-        if not module.ispkg:
-            config_path = f"{module.module_finder.path}/config.json"
-
-            if os.path.exists(config_path):
-                yield config_path
-
-
 def get_extension(extension_name):
     for module in pkgutil.walk_packages(extensions.__path__, f"{extensions.__name__}."):
         if not module.ispkg and module.name.split(".")[-1] == extension_name:
