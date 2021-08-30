0ab5cd861ba1606569e19f68955577c4589fdf71
Simon Roy <simon.roy1211@gmail.com>
2021-08-29 20:35:19 -0400

    Fix for mysql

--- bot/models/bot.py
+++ bot/models/bot.py
@@ -9,10 +9,10 @@ class Bot(app.base, Model):
     __tablename__ = "bots"
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, unique=True)
-    prefix = Column(String, nullable=False)
-    description = Column(String)
-    default_color_code = Column(String)
+    name = Column(String(255), unique=True)
+    prefix = Column(String(255), nullable=False)
+    description = Column(String(255))
+    default_color_code = Column(String(255))
 
-    welcome_message = Column(String)
+    welcome_message = Column(String(255))
     channels = relationship("BotChannel", cascade="all")
--- bot/models/bot_channel.py
+++ bot/models/bot_channel.py
@@ -7,7 +7,7 @@ class BotChannel(app.base, Model):
     __tablename__ = 'bot_channels'
 
     bot_id = Column(ForeignKey("bots.id"), primary_key=True)
-    channel_name = Column(String, primary_key=True)
+    channel_name = Column(String(255), primary_key=True)
     channel_id = Column(BigInteger, primary_key=True)
 
     UniqueConstraint("bot_id", "channel_name", "channel_id", name="uq_bid_cn_cid")
