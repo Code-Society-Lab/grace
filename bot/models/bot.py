9e9d9f5102159fc8bbc6ba5279edb9261f17baf0
Simon Roy <simon.roy1211@gmail.com>
2021-08-30 20:50:59 -0400

    Hot fix for welcome message not working --- NONE

--- bot/models/bot.py
+++ bot/models/bot.py
@@ -1,7 +1,11 @@
+from logging import info
+
+import sqlalchemy.orm.exc
 from sqlalchemy import String, Column, Integer
 from sqlalchemy.orm import relationship
 
 from bot import app
+from bot.models.bot_channel import BotChannel
 from db.model import Model
 
 
@@ -16,3 +20,41 @@ class Bot(app.base, Model):
 
     welcome_message = Column(String(255))
     channels = relationship("BotChannel", cascade="all")
+
+    def get_channel(self, **kwargs):
+        """Returns the corresponding channel informations
+
+        :param **kwargs
+            Possible argument:
+                name : The channel name
+                id : The channel id
+
+        :return channel (
+            Will return 'NoneM if the the channel is not found
+
+        :raise ValueError
+            If the parameter are not valid, a 'ValueError' exception will be raised.
+
+        Ex.
+            ```
+                # Retrieve the channel with the name of the channel
+                get_channel(name="channel-name")
+
+                # Retrieve the channel with the id of the channel
+                get_channel(id="827698582861512714")
+            ```
+        """
+        channel_name = kwargs.get("name", None)
+        channel_id = kwargs.get("id", None)
+
+        if channel_name:
+            channel = BotChannel.where(bot_id=self.id, channel_name=channel_name)
+        elif channel_id:
+            channel = BotChannel.where(bot_id=self.id, channel_id=channel_id)
+        else:
+            raise ValueError("You need to pass the channel 'id' or channel 'name'")
+
+        if channel:
+            return channel.first()
+
+        return None
