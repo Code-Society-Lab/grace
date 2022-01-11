959d1058f557414b72368499996162c3c06d5293
Simon Roy <simon.roy1211@gmail.com>
2022-01-11 14:56:07 -0500

    Fix missing length for varchar

--- bot/models/extensions/fun/language/trigger_word.py
+++ bot/models/extensions/fun/language/trigger_word.py
@@ -7,4 +7,4 @@ class TriggerWord(app.base, Model):
     __tablename__ = 'trigger_words'
 
     trigger_id = Column(ForeignKey("triggers.id"), primary_key=True)
-    word = Column(String, primary_key=True)
+    word = Column(String(255), primary_key=True)
