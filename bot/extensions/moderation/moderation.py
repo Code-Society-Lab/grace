c3de520a119734c75d0b48a1cfd7d12073a26b48
Shadwz17 <shadwz17@outlook.com>
2022-01-17 14:34:44 -0300

    Added a simple moderation extension

--- bot/extensions/moderation/moderation.py
+++ bot/extensions/moderation/moderation.py
@@ -1,52 +1,52 @@
+import asyncio
 from discord import Member
-from discord.ext.commands import Cog, command, has_permissions
-from bot.helpers.log_helper import danger
+import datetime
+from json import loads
+from turtle import title
+from discord.ext.commands.cooldowns import BucketType
+from discord.ext.commands import Cog, command, cooldown, has_permissions
+from discord import Embed
 
 
 class ModerationCog(Cog, name="moderation", description="Collection of administrative use commands."):
     def __init__(self, bot):
         self.bot = bot
 
-    def get_moderation_channel(self):
-        return self.bot.get_channel_by_name("moderation_logs")
-
     @command(name='kick', help="Allows a staff member to kick a user based on their behaviour.")
+    @cooldown(1, 5, BucketType.user)
     @has_permissions(kick_members=True)
-    async def kick(self, ctx, member: Member, reason="No reason given"):
-        log = danger("KICK", f"{member.mention} has been kicked.")
-        log.add_field("Issuer: ", ctx.author)
-        log.add_field("Reason: ", reason)
-
-        await ctx.guild.kick(user=member)
-        await log.send(self.get_moderation_channel())
+    async def kick(self, ctx, member: Member , reason="No reason"):
+            guild = ctx.guild
+            embed = Embed(title="Grace Moderation - KICK", description=f"{member.mention} has been kicked.", timestamp=datetime.datetime.utcnow())
+            embed.add_field(name="Reason: ", value=str(reason), inline=False)
+            await ctx.reply(embed=embed)
+            await guild.kick(user=member)
 
     @command(name='ban', help="Allows a staff member to ban a user based on their behaviour.")
+    @cooldown(1, 5, BucketType.user)
     @has_permissions(ban_members=True)
-    async def ban(self, ctx, member: Member, reason="No reason"):
-        log = danger("BAN", f"{member.mention} has been banned.")
-        log.add_field("Issuer: ", ctx.author.mention)
-        log.add_field("Reason: ", reason)
-
-        await ctx.guild.ban(user=member)
-        await log.send(self.get_moderation_channel())
+    async def ban(self, ctx, member: Member , reason="No reason"):
+            guild = ctx.guild
+            embed = Embed(title="Grace Moderation - BAN", description=f"{member.mention} has been banned.", timestamp=datetime.datetime.utcnow())
+            embed.add_field(name="Reason: ", value=str(reason), inline=False)
+            await ctx.reply(embed=embed)
+            await guild.ban(user=member)
 
     @command(name='unban', help="Allows a staff member to unban a user.")
     @has_permissions(ban_members=True)
-    async def unban(self, ctx, user_id: int):
-        user = await self.bot.fetch_user(user_id)
-        log = danger("UNBAN", f"{user.name} has been unbanned.")
-
+    async def unban(self, ctx, id: int):
+        user = await self.bot.fetch_user(id)
         await ctx.guild.unban(user)
-        await log.send(self.get_moderation_channel())
-
+        embed = Embed(title="Grace Moderation - UNBAN", description=f"{user.name} has been unbanned.", timestamp=datetime.datetime.utcnow())
+        await ctx.reply(embed=embed)      
+        
     @command(name='purge', help="Deletes n amount of messages.")
     @has_permissions(manage_messages=True)
     async def purge(self, ctx, limit: int):
-        log = danger("PURGE", f"{limit} message(s) purged by {ctx.author.mention} in {ctx.channel.mention}")
-
         await ctx.channel.purge(limit=limit)
-        await log.send(self.get_moderation_channel())
-
+        embed = Embed(title="Grace Moderation - PURGE", description=f"Chat cleared by {ctx.author.mention}")
+        await ctx.send(embed=embed)
+        await ctx.message.delete()
 
 def setup(bot):
     bot.add_cog(ModerationCog(bot))
