656591e0cb0c1652fde20da139fce0f88e6bd6ea
Simon Roy <simon.roy1211@gmail.com>
2022-01-21 01:36:34 -0500

    Small tweak and convention fix for color cog --- NONE

--- bot/extensions/fun/color_cog.py
+++ bot/extensions/fun/color_cog.py
@@ -5,41 +5,53 @@ from bot.extensions.utils.command_error_handler import CommandErrorHandler
 from discord import Embed, File
 
 
-class ColorCog(Cog, name="Color", description="Collection of commands to display colors."):
+class ColorCog(Cog, name="Color", description="Collection of commands to for colors."):
     def __init__(self, bot):
         self.bot = bot
 
-    @group(name="show color", aliases=["color"], help="Commands to display colors.")
+    @group(name="color", help="Commands to bring color in your life")
     async def color_group(self, ctx):
         if ctx.invoked_subcommand is None:
             await CommandErrorHandler.send_command_help(ctx)
 
-    async def display_color(self, color_img, ctx):
-        color_img.save('color.png')
+    @color_group.group(name="show", help="Commands to display colors.")
+    async def show_group(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await CommandErrorHandler.send_command_help(ctx)
+
+    async def display_color(self, ctx, color):
+        colored_image = Image.new('RGB', (200, 200), color)
+        colored_image.save('color.png')
+        file = File('color.png')
 
         embed = Embed(
             color=self.bot.default_color,
-            description='Here goes your color!'
+            title='Here goes your color!',
+            description=f"{color}"
         )
-        file = File('color.png')
-        await ctx.send(embed=embed, file=file)
+        embed.set_image(url="attachment://color.png")
 
+        await ctx.send(embed=embed, file=file)
         os.remove('color.png')
 
-    @color_group.command(name='rgb', help="Displays the RGB color entered by the user." usage="color rgb {red integer} {green integer} {blue integer}")
+    @show_group.command(
+        name='rgb',
+        help="Displays the RGB color entered by the user.",
+        usage="color show rgb {red integer} {green integer} {blue integer}"
+    )
     async def rgb_command(self, ctx, r: int, g: int, b: int):
-        img = Image.new('RGB', (200, 200), (r, g, b))
+        await self.display_color(ctx, (r, g, b))
 
-        await self.display_color(img, ctx)
-
-    @color_group.command(name='hex', help="Displays the color of the hexcode entered by the user.", usage="color hex {hexadecimal string}"))
+    @show_group.command(
+        name='hex',
+        help="Displays the color of the hexcode entered by the user.",
+        usage="color hex {hexadecimal string}"
+    )
     async def hex_command(self, ctx, hex: str):
         if not hex.startswith('#'):
             hex = f'#{hex}'
 
-        img = Image.new('RGB', (200, 200), hex)
-
-        await self.display_color(img, ctx)
+        await self.display_color(ctx, hex)
 
 
 def setup(bot):
