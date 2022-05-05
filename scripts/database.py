f8acc7e17423d1b691528e189306893cee501321
Simon Roy <simon.roy1211@gmail.com>
2022-05-05 01:46:02 -0400

    Fix quirks of grace configuration, environment and startup

--- scripts/database.py
+++ scripts/database.py
@@ -6,14 +6,11 @@ This module contains the additional database commands for the `grace` script.
 from logging import warning, critical, info, error
 from sqlalchemy.exc import ProgrammingError, SQLAlchemyError, IntegrityError
 from bot import app
-from utils.models import load_models
 from db.seed import get_seeds
-
-load_models()
 
 
 def seed_tables():
-    warning("Seeding tables...")
+    info("Seeding tables...")
 
     try:
         try:
@@ -23,19 +20,19 @@ def seed_tables():
         except IntegrityError as e:
             error(f"Error: {e}, Skipping")
 
-        warning("Seeding completed successfully")
+        info("Seeding completed successfully")
     except ProgrammingError as e:
         critical(f"Critical Error: {e}")
 
 
 def create_all():
-    warning(f"Creating all...")
+    info(f"Creating all...")
 
     try:
         app.create_database()
         app.create_tables()
 
-        warning("Database created successfully!")
+        info("Database created successfully!")
     except SQLAlchemyError as e:
         critical(f"Critical Error: {e}")
 
@@ -47,7 +44,7 @@ def delete_all():
         app.drop_tables()
         app.drop_database()
 
-        warning("Database deleted successfully!")
+        info("Database deleted successfully!")
     except SQLAlchemyError as e:
         critical(f"Critical Error: {e}")
 
