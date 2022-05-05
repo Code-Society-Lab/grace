35dd165e88c375be7c466cfe36bf3e1c3f702321
Simon Roy <simon.roy1211@gmail.com>
2022-05-05 03:08:53 -0400

    Introduction to bot settings (will replace the bot sql config)

---
+++ config/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    NAME = "grace"
+    PREFIX = "::"
+    DESCRIPTION = "Grace is the official Code Society Discord bot"
+    DEFAULT_COLOR = 0xfffffe
+    WELCOME_MESSAGE = "Hi {member_name}! Welcome to the **Code Society**.\n\nBefore posting please:\n    - Take a " \
+                      "moment to read the <#{info_id}> and the <#{rules_id}>.\n    - Choose some <#{roles_id}>.\n    " \
+                      "- Feel free to introduce yourself in <#{intro_id}>. "
