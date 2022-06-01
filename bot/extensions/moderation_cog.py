245f5b153245f35ebbfc53ea105a314acd34461a
Simon Roy <simon.roy1211@gmail.com>
2022-06-01 02:08:28 -0400

    Added ability to purge message from a specific user

--- bot/extensions/moderation_cog.py
+++ bot/extensions/moderation_cog.py
@@ -3,7 +3,7 @@ from discord.ext.commands import Cog, command, has_permissions
 from bot.helpers.log_helper import danger
 
 
-class ModerationCog(Cog, name="moderation", description="Collection of administrative use commands."):
+class ModerationCog(Cog, name="moderation", description="Collection of administrative commands."):
     def __init__(self, bot):
         self.bot = bot
 
@@ -41,10 +41,15 @@ class ModerationCog(Cog, name="moderation", description="Collection of administr
 
     @command(name='purge', help="Deletes n amount of messages.")
     @has_permissions(manage_messages=True)
-    async def purge(self, ctx, limit: int):
+    async def purge(self, ctx, limit: int, member: Member = None):
         log = danger("PURGE", f"{limit} message(s) purged by {ctx.author.mention} in {ctx.channel.mention}")
 
-        await ctx.channel.purge(limit=limit)
+        if member:
+            async for message in ctx.channel.history(limit=limit):
+                if message.author == member:
+                    await message.delete()
+        else:
+            await ctx.channel.purge(limit=limit)
         await log.send(self.get_moderation_channel())
 
 
