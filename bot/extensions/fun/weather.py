3ee1930e91c7c2cf5011248255f75d4bd8f36c5a
Chris Dedman-Rollet <61106361+chrisdedman@users.noreply.github.com>
2022-06-13 18:28:14 -0700

    edited
    Edited based on the current comment

--- bot/extensions/fun/weather.py
+++ bot/extensions/fun/weather.py
@@ -1,129 +1,104 @@
 from geopy.geocoders import Nominatim
 from timezonefinder import TimezoneFinder
-import pytz
 from pytz import country_timezones, timezone
 from datetime import datetime
 from discord.ext.commands import Cog, command
-import requests
+from requests import get
 from discord import Embed
-import time
-import string
+from string import capwords
 
-from discord.utils import parse_time
 
-class WeatherCog(Cog):
+class WeatherCog(Cog, name="Weather", description="get current weather information from a city"):
     def __init__(self, bot):
         self.bot = bot
-
-    @command(name='weather', help='Show weather information in your city', usage="{city}")
-    async def my_command(self, ctx, *city_input):
-        # get all argument inputed by the user 
-        # on discord and join them together 
-        # (handle double-words/argument cities)
-        city = " "
-        city = string.capwords(city.join(city_input))
     
+    @command(name='weather', help='Show weather information in your city', usage="{city}")
+    async def weather(self, ctx, *city_input):
+        city = capwords(" ".join(city_input))
+
         # initialize Nominatim API
         geolocator = Nominatim(user_agent="geoapiExercises")
-        # city_name = city.capitalize()
+        
         # getting Latitude and Longitude
         location = geolocator.geocode(city)
-    
+
         # pass the Latitude and Longitude
         # into a timezone_at
         # and it return timezone
-        obj = TimezoneFinder()
+        timezone_finder = TimezoneFinder()
 
-        # returns timezone
-        result = obj.timezone_at(lng=location.longitude, lat=location.latitude)
-        timezone_city = datetime.now(pytz.timezone(result))
+        result = timezone_finder.timezone_at(
+            lng=location.longitude, 
+            lat=location.latitude)
+        timezone_city = datetime.now(timezone(result))
         timezone_city = timezone_city.strftime('%m/%d/%Y %H:%M')
 
-        # Enter your API key here
-        api_key="441df3a5cadc2498e093c0367cae6817"
-        # base_url variable to store url
+        api_key = "441df3a5cadc2498e093c0367cae6817"
         base_url = "http://api.openweathermap.org/data/2.5/weather?"
-        # Give city name
 
-        city_name = str(city)
-        # complete_url variable to store complete url address
-        complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-        # get method of requests module return response object
-        response = requests.get(complete_url)
-        # json method of response object convert json format data into python format data
+        # complete_url to retreive weather info
+        complete_url = base_url + "appid=" + api_key + "&q=" + city
+        response = get(complete_url)
         data_weather = response.json()
 
-        # Now data_weather contains list of nested dictionaries, check the value of "cod" key is not equal to
-        # "404", means city is found otherwise, city is not found
-        if data_weather["cod"] != "404":
-            # get icon id
-            icon_id = data_weather["weather"][0]["icon"]
-            # store the value of "main" key in variable main
-            main = data_weather["main"]
-            # store the value of visibility into the variable visibility
-            visibility = data_weather['visibility']
-            # store the value corresponding to the "temp" key of main
-            current_temperature = main["temp"]
-            # Convert in Fahrenheit
-            fahrenheit = round((int(current_temperature)) * 1.8 - 459.67,2)
-            celsius = round((int(current_temperature) - 273.15), 2)
-            # store the value corresponding to the "pressure" key of main
-            current_pressure = main["pressure"]
-            # store the value corresponding to the "humidity" key of main
-            current_humidiy = main["humidity"]
-            # store the value of "weather" key in variable forcast
-            forcast = data_weather["weather"]
-            # store the value corresponding to the "description" key at the 0th index of forcast
+        # Now data_weather contains list of nested dictionaries, 
+        # check the value of "cod" if key is equal to 200
+        # it means the city is found otherwise, city is not found
+        if data_weather["cod"] == 200:
+            icon_id = data_weather["weather"][0]["icon"]    # get image icon id
+            main = data_weather["main"]                     # get the main datas
+            visibility = data_weather['visibility']         # get current visibility
+            current_temperature = main["temp"]              # get current temperature  
+            fahrenheit = round((int(current_temperature)) * 1.8 - 459.67, 2)    # convert kelvin to fahrenheit
+            celsius = round((int(current_temperature) - 273.15), 2)             # convert kelvin to celsius
+            current_pressure = main["pressure"]             # get current pressure athmospheric
+            current_humidity = main["humidity"]             # get current humidity
+            forcast = data_weather["weather"]               # get current weather description
             weather_description = forcast[0]["description"]
-            # print following values
+            
             embed = Embed(
-            color = self.bot.default_color,
-            title = f"{city}",
+                color=self.bot.default_color,
+                title=f"{city}",
                 description=f"{timezone_city}",
             )
-        
+
             embed.set_image(
-            url='http://openweathermap.org/img/wn/' + icon_id + '@2x.png'
+                url='http://openweathermap.org/img/wn/' 
+                + icon_id 
+                +'@2x.png'
             )
             embed.add_field(
                 name="Description",
-                value=f"{str(string.capwords(weather_description))}"
-                "\u200b",
+                value=f"{capwords(weather_description)}",
                 inline=False
             )
             embed.add_field(
                 name="Visibility",
-                value=f"{visibility}m | {round(visibility*3.280839895)}ft"
-                "\u200b",
+                value=f"{visibility}m | {round(visibility*3.280839895)}ft",
                 inline=False
             )
             embed.add_field(
-            name = "Temperature",
-                value=f"{fahrenheit}°F | {celsius}°C"
-                    "\u200b",
-            inline = False
+                name="Temperature",
+                value=f"{fahrenheit}°F | {celsius}°C",
+                inline=False
             )
             embed.add_field(
-            name = "Atmospheric Pressure",
-            value = f"{current_pressure} hPa"
-                    "\u200b",
-            inline = False
+                name="Atmospheric Pressure",
+                value=f"{current_pressure} hPa",
+                inline=False
             )
             embed.add_field(
-            name = "Humidity",
-            value = f"{current_humidiy}%"
-                    "\u200b",
-            inline = False
+                name="Humidity",
+                value=f"{current_humidity}%",
+                inline=False
             )
-
-            await ctx.send(embed=embed)
         else:
-            embeded = Embed(
+            embed = Embed(
                 color=self.bot.default_color,
-                # title=f"{city}",
-                description=f"{city.capitalize()} No Found!",
+                description=f"{city} No Found!",
             )
-            await ctx.send(embed=embeded)
+        await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(WeatherCog(bot))
