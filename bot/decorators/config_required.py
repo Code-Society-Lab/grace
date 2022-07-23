3b376648a4b24cb46e864cfd8f640bf14d6a858d
Simon Roy <simon.roy1211@gmail.com>
2022-07-23 10:21:35 -0400

    Added comments for config required decorator

--- bot/decorators/config_required.py
+++ bot/decorators/config_required.py
@@ -1,21 +1,43 @@
 from bot import app
-from discord.ext.commands import CommandError
 from discord.ext import commands
+from discord.ext.commands import CommandError, CogMeta
 
 
 class ConfigRequiredError(CommandError):
+    """The base exception type for errors to required config check
+
+    Inherit from `discord.ext.commands.CommandError` and can be handled like
+    other CommandError exception in `on_command_error`
+    """
     pass
 
 
 class MissingRequiredConfig(ConfigRequiredError):
+    """Exception raised when a required configuration is missing.
+
+    Inherit from `ConfigRequiredError`
+    """
     def __init__(self, section_key, value_key):
         super().__init__(f"Missing config '{value_key}' in section '{section_key}'")
 
 
 def cog_config_required(section_key, value_key):
+    """Validates the presences of a given configuration before each
+    invocation of a `discord.ext.commands.Cog` commands
+
+    :param section_key:
+        The required section key
+    :param value_key:
+        The required value key
+    :raises TypeError:
+        If the class is not a Cog
+    """
     def decorator(cls):
         class Wrapper(cls):
             def __init__(self, *args, **kargs):
+                if not isinstance(cls, CogMeta):
+                    raise TypeError("The class needs to be a cog")
+
                 self.required_config = app.config.get(section_key, value_key)
                 super(Wrapper, self).__init__(*args, **kargs)
 
@@ -27,6 +49,14 @@ def cog_config_required(section_key, value_key):
 
 
 def command_config_required(section_key, value_key):
+    """Validates the presences of a given configuration before running
+    the `discord.ext.commands.Command`
+
+    :param section_key:
+        The required section key
+    :param value_key:
+        The required value key
+    """
     async def predicate(ctx):
         if not app.config.get(section_key, value_key):
             raise MissingRequiredConfig(section_key, value_key)
