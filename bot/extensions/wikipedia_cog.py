50584e79e9d55f478f07f65d40523b0a6ebe7015
Discord Digital <56228439+DiscordDigital@users.noreply.github.com>
2022-09-11 21:51:07 +0200

    Enhanced code through suggestions

--- bot/extensions/wikipedia_cog.py
+++ bot/extensions/wikipedia_cog.py
@@ -1,19 +1,19 @@
 from discord.ext.commands import Cog, hybrid_command
 import discord
-import urllib.request
-import json
+from urllib.request import urlopen
+from urllib.parse import quote_plus
+from json import loads
 
 
 def search_results(search):
-        urlencode = urllib.parse.quote(search)
+        url_encode = quote_plus(search)
         url = "https://en.wikipedia.org/w/api.php?action=opensearch" \
-              "&format=json&limit=3&namespace=0&search=" + urlencode
-        with urllib.request.urlopen(url) as url:
-            jsonPlaintext = url.read()
-            return json.loads(jsonPlaintext)
+              "&format=json&limit=3&namespace=0&search=" + url_encode
+        with urlopen(url) as url:
+            return loads(url.read())
 
 
-class buttons(discord.ui.View):
+class Buttons(discord.ui.View):
     def __init__(self, search, result):
         super().__init__()
         self.search = search
@@ -21,12 +21,12 @@ class buttons(discord.ui.View):
         self.value = None
 
     @discord.ui.button(label='1', style=discord.ButtonStyle.primary)
-    async def choiceOne(self, interaction: discord.Interaction,
-                        button: discord.ui.Button):
+    async def first_wiki_result(self, interaction: discord.Interaction,
+                                button: discord.ui.Button):
         if (len(self.result[3]) >= 1):
-            await interaction.response.send_message(interaction.user.mention +
-                                                    " requested:\n" +
-                                                    self.result[3][0])
+            await interaction.response.send_message(interaction.user.mention
+                                                    + " requested:\n"
+                                                    + self.result[3][0])
             self.value = True
             self.stop()
         else:
@@ -34,12 +34,12 @@ class buttons(discord.ui.View):
                                                     ephemeral=True)
 
     @discord.ui.button(label='2', style=discord.ButtonStyle.primary)
-    async def choiceTwo(self, interaction: discord.Interaction,
-                        button: discord.ui.Button):
+    async def second_wiki_result(self, interaction: discord.Interaction,
+                                 button: discord.ui.Button):
         if (len(self.result[3]) >= 2):
-            await interaction.response.send_message(interaction.user.mention +
-                                                    " requested:\n" +
-                                                    self.result[3][1])
+            await interaction.response.send_message(interaction.user.mention
+                                                    + " requested:\n"
+                                                    + self.result[3][1])
             self.value = True
             self.stop()
         else:
@@ -47,12 +47,12 @@ class buttons(discord.ui.View):
                                                     ephemeral=True)
 
     @discord.ui.button(label='3', style=discord.ButtonStyle.primary)
-    async def choiceThree(self, interaction: discord.Interaction,
-                          button: discord.ui.Button):
+    async def third_wiki_result(self, interaction: discord.Interaction,
+                                button: discord.ui.Button):
         if (len(self.result[3]) == 3):
-            await interaction.response.send_message(interaction.user.mention +
-                                                    " requested:\n" +
-                                                    self.result[3][2])
+            await interaction.response.send_message(interaction.user.mention
+                                                    + " requested:\n"
+                                                    + self.result[3][2])
             self.value = True
             self.stop()
         else:
@@ -69,17 +69,18 @@ class Wikipedia(Cog, name="Wikipedia", description="Search on Wikipedia."):
                     "from Wikipedia.")
     async def wiki(self, ctx, search: str):
         result = search_results(search)
-        view = buttons(search, result)
+        view = Buttons(search, result)
         if (len(result[1]) == 0):
             await ctx.interaction.response.send_message("No result found.",
                                                         ephemeral=True)
         else:
             resultView = ""
-            i = 0
+            search_count = 0
             for result in result[1]:
-                i += 1
-                resultView += str(i) + ": " + result + "\n"
+                search_count += 1
+                resultView += str(search_count) + ": " + result + "\n"
             await ctx.send(resultView, view=view, ephemeral=True)
+
 
 async def setup(bot):
     await bot.add_cog(Wikipedia(bot))
