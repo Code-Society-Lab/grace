85ee2ec5902a5dcad4789ed02322fa382222f9d9
Chris Dedman-Rollet <61106361+chrisdedman@users.noreply.github.com>
2022-09-30 15:43:12 -0700

    Updated class buttons redundancy, as well as PEP8.
    Applied the recommended comments made on this PR. The author abandoned the project, so we decided to finish it ourselves. I remove the redundancy within the class Buttons, some formatting as well as major PEP8 issues.

--- bot/extensions/wikipedia_cog.py
+++ bot/extensions/wikipedia_cog.py
@@ -7,8 +7,7 @@ from json import loads
 
 def search_results(search):
         url_encode = quote_plus(search)
-        url = "https://en.wikipedia.org/w/api.php?action=opensearch" \
-              "&format=json&limit=3&namespace=0&search=" + url_encode
+        url = f"https://en.wikipedia.org/w/api.php?action=opensearch&format=json&limit=3&namespace=0&search={url_encode}"
         with urlopen(url) as url:
             return loads(url.read())
 
@@ -18,68 +17,49 @@ class Buttons(discord.ui.View):
         super().__init__()
         self.search = search
         self.result = result
-        self.value = None
+
+    async def wiki_result(self, interaction, button, index):
+         if len(self.result[3]) >= index:
+            await interaction.response.send_message("{mention} requested:\n {request}".format(
+              mention=interaction.user.mention,
+              request=self.result[3][index-1]
+            ))
+            self.stop()
+        else:
+            await interaction.response.send_message("Invalid choice.", ephemeral=True)         
 
     @discord.ui.button(label='1', style=discord.ButtonStyle.primary)
-    async def first_wiki_result(self, interaction: discord.Interaction,
-                                button: discord.ui.Button):
-        if (len(self.result[3]) >= 1):
-            await interaction.response.send_message(interaction.user.mention
-                                                    + " requested:\n"
-                                                    + self.result[3][0])
-            self.value = True
-            self.stop()
-        else:
-            await interaction.response.send_message("Invalid choice.",
-                                                    ephemeral=True)
+    async def first_wiki_result(self, interaction, button):
+      await self.wiki_result(interaction, button, 1)
 
     @discord.ui.button(label='2', style=discord.ButtonStyle.primary)
-    async def second_wiki_result(self, interaction: discord.Interaction,
-                                 button: discord.ui.Button):
-        if (len(self.result[3]) >= 2):
-            await interaction.response.send_message(interaction.user.mention
-                                                    + " requested:\n"
-                                                    + self.result[3][1])
-            self.value = True
-            self.stop()
-        else:
-            await interaction.response.send_message("Invalid choice.",
-                                                    ephemeral=True)
+    async def second_wiki_result(self, interaction, button):
+      await self.wiki_result(interaction, button, 2)
 
     @discord.ui.button(label='3', style=discord.ButtonStyle.primary)
-    async def third_wiki_result(self, interaction: discord.Interaction,
-                                button: discord.ui.Button):
-        if (len(self.result[3]) == 3):
-            await interaction.response.send_message(interaction.user.mention
-                                                    + " requested:\n"
-                                                    + self.result[3][2])
-            self.value = True
-            self.stop()
-        else:
-            await interaction.response.send_message("Invalid choice.",
-                                                    ephemeral=True)
+    async def third_wiki_result(self, interaction, button):
+      await self.wiki_result(interaction, button, 3)
 
 
 class Wikipedia(Cog, name="Wikipedia", description="Search on Wikipedia."):
     def __init__(self, bot):
         self.bot = bot
 
-    @hybrid_command(
-        description="Searches and displays the first 3 results "
-                    "from Wikipedia.")
+    @hybrid_command(description="Searches and displays the first 3 results from Wikipedia.")
     async def wiki(self, ctx, search: str):
         result = search_results(search)
         view = Buttons(search, result)
-        if (len(result[1]) == 0):
-            await ctx.interaction.response.send_message("No result found.",
-                                                        ephemeral=True)
+        
+        if len(result[1]) == 0:
+            await ctx.interaction.response.send_message("No result found.", ephemeral=True)
         else:
-            resultView = ""
+            result_view = ""
             search_count = 1
             for result in result[1]:
-                resultView += str(search_count) + ": " + result + "\n"
+                result_view  += f"{str(search_count)}: {result}\n"
                 search_count += 1
-            await ctx.send(resultView, view=view, ephemeral=True)
+                
+            await ctx.send(result_view, view=view, ephemeral=True)
 
 
 async def setup(bot):
