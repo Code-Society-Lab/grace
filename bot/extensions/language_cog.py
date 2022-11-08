208cbb303b9f081d48b01a77658e9d1e8a22a14c
Simon Roy <49426163+PenguinBoi12@users.noreply.github.com>
2022-11-08 18:17:52 -0500

    Revert "Grace now says puns"

--- bot/extensions/language_cog.py
+++ bot/extensions/language_cog.py
@@ -3,8 +3,6 @@ from discord import Message, Embed
 from nltk.tokenize import TweetTokenizer
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from bot.models.extensions.language.trigger import Trigger
-from bot.models.extensions.language.pun import Pun
-from bot.models.extensions.language.pun_word import PunWord
 
 
 class LanguageCog(Cog, name="Language", description="Analyze and reacts to messages"):
@@ -40,9 +38,8 @@ class LanguageCog(Cog, name="Language", description="Analyze and reacts to messa
         linus_trigger = Trigger.get_by(name="Linus")
 
         message_tokens = self.tokenizer.tokenize(message.content)
-        tokenlist = list(map(str.lower, message_tokens))
-        linustarget = [i for i, x in enumerate(
-            tokenlist) if x in linus_trigger.words]
+        tokenlist = list(map(lambda s: s.lower(), message_tokens))
+        linustarget = [i for i, x in enumerate(tokenlist) if x in linus_trigger.words]
         # Get the indices of all linuses in the message
 
         if linustarget:
@@ -73,39 +70,10 @@ class LanguageCog(Cog, name="Language", description="Analyze and reacts to messa
             if not fail:
                 await message.add_reaction(linus_trigger.positive_emoji)
 
-    async def pun_react(self, message: Message):
-        message_tokens = self.tokenizer.tokenize(message.content)
-        tokenlist = set(map(lambda s: s.lower(), message_tokens))
-
-        pun_words = PunWord.all()
-        word_set = set([pun_word.word for pun_word in pun_words])
-
-        matches = tokenlist.intersection(word_set)
-
-        if len(matches) > 0:
-            matched_pun_words = [
-                pun_word for pun_word in pun_words if pun_word.word in matches]
-
-            puns = set([Pun.get(pun_word.pun_id)
-                       for pun_word in matched_pun_words])
-
-            for pun_word in matched_pun_words:
-                await message.add_reaction(pun_word.emoji())
-
-            for pun in puns:
-                embed = Embed(
-                    color=self.bot.default_color,
-                    title=f"Gotcha",
-                    description=pun.text
-                )
-
-                await message.channel.send(embed=embed)
-
     @Cog.listener()
     async def on_message(self, message):
         await self.penguin_react(message)
         await self.name_react(message)
-        await self.pun_react(message)
 
     @hybrid_group(name="triggers", help="Commands to manage triggers")
     @has_permissions(administrator=True)
