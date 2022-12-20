22951e334fef38f2c75903d72feab84e82275e5b
Simon Roy <simon.roy1211@gmail.com>
2022-12-20 16:16:08 -0500

    Fix formatting

--- bot/extensions/bookmark_cog.py
+++ bot/extensions/bookmark_cog.py
@@ -1,14 +1,14 @@
 from enum import Enum
 from typing import Dict, Union
 from discord import (
-	Member,
-	Embed,
-	RawReactionActionEvent,
-	GroupChannel,
-	DMChannel,
-	Thread,
-	TextChannel,
-	Message
+    Member,
+    Embed,
+    RawReactionActionEvent,
+    GroupChannel,
+    DMChannel,
+    Thread,
+    TextChannel,
+    Message
 )
 from discord.ext.commands import Cog, hybrid_group, Context
 from bot.extensions.command_error_handler import send_command_help
@@ -18,65 +18,68 @@ ChannelType = Union[TextChannel, Thread, DMChannel, GroupChannel]
 
 
 class Mode(Enum):
-	NORMAL = 0
-	SAVE = 1
+    NORMAL = 0
+    SAVE = 1
 
 
 class BookmarkCog(Cog):
-	def __init__(self, bot: Grace) -> None:
-		self.bot = bot
-		self.mode_by_user: Dict[Member, Mode] = {}
+    def __init__(self, bot: Grace) -> None:
+        self.bot = bot
+        self.mode_by_user: Dict[Member, Mode] = {}
 
-	@Cog.listener()
-	async def on_raw_reaction_add(self, payload: RawReactionActionEvent):
-		if payload.member not in self.mode_by_user:
-			return
+    @Cog.listener()
+    async def on_raw_reaction_add(self, payload: RawReactionActionEvent):
+        if payload.member not in self.mode_by_user:
+            return
 
-		if self.mode_by_user[payload.member] == Mode.SAVE:
-			if payload.emoji.name == '📖':
-				channel: ChannelType  = await self.bot.fetch_channel(payload.channel_id)
-				message: Message = await channel.fetch_message(payload.message_id)
-				save_embed: Embed = Embed(
-					title='Saved message',
-					description=f'Message sent by: **{message.author}**\n'
-					            f'Sent at: **{message.created_at.strftime("%m.%d.%Y %H:%M:%S %Z%z")}**\n'
-					            f'Message: **{message.content}**',
-				)
-				await message.remove_reaction(payload.emoji, payload.member)
-				await payload.member.send(embed=save_embed)
+        if self.mode_by_user[payload.member] == Mode.SAVE:
+            if payload.emoji.name == '📖':
+                channel: ChannelType = await self.bot.fetch_channel(payload.channel_id)
+                message: Message = await channel.fetch_message(payload.message_id)
 
-	@hybrid_group(name='bookmark')
-	async def bookmark(self, ctx: Context):
-		if ctx.invoked_subcommand is None:
-			await send_command_help(ctx)
+                print(message.content)
+                save_embed: Embed = Embed(
+                    color=self.bot.default_color,
+                    title='Saved message',
+                    description="Message sent by: **{author}**\nSent at: **{sent_at}**\nMessage: **{content}**".format(
+                        author=message.author,
+                        sent_at=message.created_at.strftime("%m/%d/%Y %H:%M:%S"),
+                        content=message.content
+                    )
+                )
+                await message.remove_reaction(payload.emoji, payload.member)
+                await payload.member.send(embed=save_embed)
 
-	@bookmark.command(name='save', description='Enters into a save mode where you can start saving messages')
-	async def save_mode(self, ctx: Context):
-		save_mode_embed: Embed = Embed(
-			title='SAVE MODE',
-			description='You have entered the **Save Mode**.\n'
-			            'You can save messages by reacting with 📖 **emoji** on them\n'
-			            'To **exit** the save mode, use this command: _/bookmark exit_',
-			color=self.bot.default_color
-		)
+    @hybrid_group(name='bookmark')
+    async def bookmark(self, ctx: Context):
+        if ctx.invoked_subcommand is None:
+            await send_command_help(ctx)
 
-		self.mode_by_user[ctx.author] = Mode.SAVE
+    @bookmark.command(name='save', description='Enters into a save mode where you can start saving messages')
+    async def save_mode(self, ctx: Context):
+        save_mode_embed: Embed = Embed(
+            title='SAVE MODE',
+            description='You have entered the **Save Mode**.\n'
+                        'You can save messages by reacting with 📖 **emoji** on them\n'
+                        'To **exit** the save mode, use this command: _/bookmark exit_',
+            color=self.bot.default_color
+        )
 
-		await ctx.reply(embed=save_mode_embed, ephemeral=True)
+        self.mode_by_user[ctx.author] = Mode.SAVE
 
-	@bookmark.command(name='exit', description='Enters into a normal mode')
-	async def save_mode(self, ctx: Context):
-		normal_mode_embed: Embed = Embed(
-			title='NORMAL MODE',
-			description='You are back in the **Normal Mode**.',
-			color=self.bot.default_color
-		)
+        await ctx.reply(embed=save_mode_embed, ephemeral=True)
 
-		self.mode_by_user[ctx.author] = Mode.NORMAL
+    @bookmark.command(name='exit', description='Enters into a normal mode')
+    async def save_mode(self, ctx: Context):
+        normal_mode_embed: Embed = Embed(
+            title='NORMAL MODE',
+            description='You are back in the **Normal Mode**.',
+            color=self.bot.default_color
+        )
+        self.mode_by_user[ctx.author] = Mode.NORMAL
 
-		await ctx.reply(embed=normal_mode_embed, ephemeral=True)
+        await ctx.reply(embed=normal_mode_embed, ephemeral=True)
 
 
 async def setup(bot: Grace):
-	await bot.add_cog(BookmarkCog(bot))
-	
+    await bot.add_cog(BookmarkCog(bot))
