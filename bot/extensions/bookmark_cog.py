f028dd3c4085a4da5ecb2e4ff0e10cd5bbece8c6
Simon Roy <simon.roy1211@gmail.com>
2022-12-23 14:25:48 -0500

    Adjustement of info display

--- bot/extensions/bookmark_cog.py
+++ bot/extensions/bookmark_cog.py
@@ -2,47 +2,51 @@ from typing import List
 from discord import Embed, Message, Interaction, File
 from discord.app_commands import ContextMenu
 from discord.ext.commands import Cog
-from bot.extensions.command_error_handler import send_command_help
 from bot.grace import Grace
-from datetime import timedelta
 
 
 class BookmarkCog(Cog):
 	def __init__(self, bot: Grace) -> None:
 		self.bot: Grace = bot
 
-		save_message_ctx_menu = ContextMenu(
+		save_message_ctx_menu: ContextMenu = ContextMenu(
 			name='Save Message',
 			callback=self.save_message
 		)
 
 		self.bot.tree.add_command(save_message_ctx_menu)
-	
-	async def fetch_files(self, message: Message) -> List[File]:
+
+	async def get_message_files(self, message: Message) -> List[File]:
 		"""Fetch files from the message attachments
 		
 		:param message: Message to fetch files from
+		:type message: Message
+
+		:return: List of files
+		:rtype: List[File]
 		"""
-		files = []
-		for attachment in message.attachments:
-			files.append(await attachment.to_file())
-		return files
+		return list(map(lambda attachment: attachment.to_file(), message.attachments))
 
 	async def save_message(self, interaction: Interaction, message: Message) -> None:
 		"""Saves the message
 		
 		:param interaction: ContextMenu command interaction
+		:type interaction: Interaction
 		:param message: Message of the interaction
+		:type message: Message
 		"""
-		date: int = int(message.created_at.timestamp())
-		files = await self.fetch_files(message)
+		sent_at: int = int(message.created_at.timestamp())
+		files = await self.get_message_files(message)
+
 		save_embed: Embed = Embed(
-			title='Save Info',
-			description=f'Message sent by: **{message.author}**\n'
-						f'Sent at: **<t:{date}>**\n'
-						f':arrow_down:',
+			title='Bookmark Info',
 			color=self.bot.default_color
 		)
+
+		save_embed.add_field(name="Sent By", value=message.author, inline=False)
+		save_embed.add_field(name="Sent At", value=f'<t:{sent_at}>', inline=False)
+		save_embed.add_field(name="Original Message", value=f'[Jump]({message.jump_url})', inline=False)
+
 		await interaction.user.send(embed=save_embed)
 		await interaction.user.send(message.content, embeds=message.embeds, files=files)
 		await interaction.response.send_message("Message successfully saved.", ephemeral=True)
