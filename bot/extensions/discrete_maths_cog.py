527fdbd9b814d9bd94c897c6da82d83f87dfba21
Retr05041 <parker@cranfield.ca>
2023-07-27 13:43:15 -0700

    Add Tautology checker command + Error handler

--- bot/extensions/discrete_maths_cog.py
+++ bot/extensions/discrete_maths_cog.py
@@ -3,8 +3,9 @@
 
 from discord.ext.commands import Cog, hybrid_group, Context
 from bot.extensions.command_error_handler import CommandErrorHandler
+from bot.helpers.error_helper import get_original_exception
 
-from lib.discrete_maths import DiscreteMaths
+import lib.discrete_maths.logic_operations as logic_operations
 
 # A cog for discrete maths commands
 class DiscreteMathsCog(Cog, name="Discrete Maths", description="Run discrete maths commands."):
@@ -18,11 +19,25 @@ class DiscreteMathsCog(Cog, name="Discrete Maths", description="Run discrete mat
         if ctx.invoked_subcommand is None:
             await CommandErrorHandler.send_command_help(ctx)
     
-    # Main command
+    # Truth table generator
     @discrete_maths_group.command(name="truth_table", aliases=['tt'], help="Input a proposition and get a truth table. example: \"(~A v B) ^ S\"", usage="\"{proposition}\"")
     async def truth_table_command(self, ctx: Context, proposition: str) -> None:
-        await ctx.send(f"Truth table: ```{DiscreteMaths().create_truth_table(proposition)}```")
-    
+        await ctx.send(f"Truth table: ```{logic_operations.create_truth_table(proposition)}```")
+
+    # Tautology checker
+    @discrete_maths_group.command(name="is_tautology", aliases=['it'], help="Input a proposition and check if it is a tautology. example: \"(~A v B) ^ S\"", usage="\"{proposition}\"")
+    async def tautology_checker_command(self, ctx: Context, proposition: str) -> None:
+        if (logic_operations.is_prop_tautolgy(proposition)):
+            await ctx.send(f"It's a tautology.")
+        else:
+            await ctx.send(f"It's not a tautology.")
+
+    # Error handler
+    @truth_table_command.error
+    @tautology_checker_command.error
+    async def discrete_maths_command_error(self, ctx: Context, error) -> None:
+        error = get_original_exception(error)
+        await ctx.send(f"Error: {error}")
     
     
 # Setup the cog
