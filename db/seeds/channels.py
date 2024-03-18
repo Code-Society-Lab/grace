e97c62e07ebe9954147a8de502ecbc2617cb23b9
Chris <rolletchris1@gmail.com>
2024-03-18 16:26:45 -0700

    added help message in welcome

--- db/seeds/channels.py
+++ db/seeds/channels.py
@@ -9,7 +9,9 @@ def seed_database():
         "info": 825404191492276225,
         "rules": 823183118902362132,
         "welcome": 823178343943897091,
-        "moderation_logs": 876592591657918514
+        "moderation_logs": 876592591657918514,
+        "help": 1019793296740073614,
+        "posting-guidelines": 1068966762730750105
     }
 
     for channel_name in initial_channels:
