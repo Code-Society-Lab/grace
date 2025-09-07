287958037998529fc5d127ab097a8cb8a38cfad0
MrNesli <nikitf655@gmail.com>
2025-09-07 23:48:10 +0200

    Refactoring, renaming

--- bot/extensions/mermaid_cog.py
+++ bot/extensions/mermaid_cog.py
@@ -1,45 +1,20 @@
-from discord.ext.commands import Cog, hybrid_command, Context
-from discord import Embed, Message
-from bot.extensions.command_error_handler import send_command_help
 from typing import Optional
 import re
-import requests
-import base64
+
+from discord.ext.commands import Cog, hybrid_command, Context
+from discord import Embed, Message
+
+from bot.extensions.command_error_handler import send_command_help
+from bot.services.mermaid_service import generate_mermaid_diagram
 
 
 class MermaidCog(Cog, name="Mermaid", description="Generates mermaid diagrams"):
     def __init__(self, bot):
         self.bot = bot
-        self.MERMAID_API = "https://mermaid.ink/img/"
         self.mermaid_codeblock_regex = re.compile(r"```mermaid([\S\s]*)```")
         self.codeblock_regex = re.compile(r"```([\S\s]*)```")
-
-    def mermaid_script_to_base64(self, script: str) -> str:
-        """ Converts script string to base64 encoding 
         
-        :param script: String to be converted
-        :type script: str
-
-        :returns: Converted string
-        :rtype: str
-        """
-        scriptbytes = script.encode("utf8")
-        base64_bytes = base64.urlsafe_b64encode(scriptbytes)
-        base64_string = base64_bytes.decode("ascii")
-        return base64_string
-
-    def get_api_url(self, script: str) -> str:
-        """ Constructs mermaid ink API url to generate mermaid diagram image
-        
-        :param script: Mermaid script from which the diagram will be generated
-        :type script: str
-
-        :returns: Mermaid ink API url
-        :rtype: str
-        """
-        return self.MERMAID_API + self.mermaid_script_to_base64(script)
-        
-    def create_mermaid_embed(self, script: str) -> Embed:
+    def generate_diagram_embed(self, diagram: str) -> Embed:
         """ First, we make sure that the API url returns OK (200) response code
         If OK, we return an embed with the image that'll be fetched from the API url
         If not OK, we return an embed with the error code
@@ -48,33 +23,34 @@ class MermaidCog(Cog, name="Mermaid", description="Generates mermaid diagrams"):
         :type script: str
         :rtype: Embed
         """
-        mermaid_api_url = self.get_api_url(script)
-
-        image_data = requests.get(mermaid_api_url)
-        status_code = image_data.status_code
-
         embed = Embed()
+        diagram_url = generate_mermaid_diagram(diagram)
 
-        print("Got status code:", status_code)
-        if status_code in [400, 404]:
+        if diagram_url:
+            embed.set_image(url=diagram_url)
+        else:
             embed.title = "Mermaid compilation error"
-            embed.description = "Unknown diagram"
-            return embed
-        elif status_code != 200:
-            embed.title = "Mermaid compilation error"
-            embed.description = f"Unknown API error. Status code: {status_code}"
-            return embed
+            embed.description = "An error occurred while generating the diagram. Please, make sure there is no syntax error.\n\nhttps://mermaid.js.org/intro/getting-started.html"
 
-        embed.set_image(url=mermaid_api_url)
         return embed
 
-    def parse_code_block(self, string: str, from_start: bool = False) -> str:
+    def parse_code_block(
+            self, 
+            string: str,
+            mermaid_block: bool = False,
+            from_start: bool = False,
+        ) -> str:
         """ Finds a code block in 'string'
 
         :example:
         ```
         THIS IS
         A CODE BLOCK
+        ```
+
+        ```mermaid
+        THIS IS
+        A MERMAID CODE BLOCK
         ```
         
         :param string: String from which the code block will be extracted
@@ -86,31 +62,16 @@ class MermaidCog(Cog, name="Mermaid", description="Generates mermaid diagrams"):
         :rtype: str
         """
         codeblock_match = None
-        if from_start:
+
+        if mermaid_block:
+            codeblock_match = self.mermaid_codeblock_regex.search(string)
+        elif from_start:
             codeblock_match = self.codeblock_regex.match(string)
         else:
             codeblock_match = self.codeblock_regex.search(string)
 
-        if codeblock_match:
-            return codeblock_match.group(1)
+        print(codeblock_match)
 
-        return ''
-
-    def parse_mermaid_code_block(self, string: str) -> str:
-        """ Finds a mermaid code block in 'string' and returns its value
-
-        :example:
-        ```mermaid
-        THIS IS A MERMAID CODE BLOCK
-        ```
-        
-        :param string: String from which the mermaid code block will be extracted
-        :type script: str
-
-        :returns: Matched code block value
-        :rtype: str
-        """
-        codeblock_match = self.mermaid_codeblock_regex.search(string)
         if codeblock_match:
             return codeblock_match.group(1)
 
@@ -132,25 +93,19 @@ class MermaidCog(Cog, name="Mermaid", description="Generates mermaid diagrams"):
         :param script_block: Mermaid script to generate the diagram image from
         :type script_block: Optional[str]
         """
-        # Mermaid command with a code block containing mermaid script
+        diagram = None
+
         if not ctx.message.reference and script_block:
-            # ::mermaid ``` <SCRIPT> ```
-            mermaid_script = self.parse_code_block(script_block, from_start=True)
-            if not mermaid_script:
-                return await send_command_help(ctx)
-
-            await ctx.reply(embed=self.create_mermaid_embed(mermaid_script))
-        # Mermaid command with no code block but with a referenced message containing mermaid code block
+            diagram = self.parse_code_block(script_block, from_start=True)
         elif ctx.message.reference and not script_block:
-            # Generate mermaid schema from referenced message
+            # Mermaid command on reply
             ref_msg = await ctx.channel.fetch_message(ctx.message.reference.message_id)
-            mermaid_script = self.parse_code_block(ref_msg.content)
-            if not mermaid_script:
-                return
+            diagram = self.parse_code_block(ref_msg.content)
 
-            await ctx.reply(embed=self.create_mermaid_embed(mermaid_script))
-        elif ctx.message.reference and script_block:
-            pass
+        if not diagram:
+            send_command_help(ctx)
+
+        await ctx.reply(embed=self.generate_diagram_embed(diagram))
 
     @Cog.listener()
     async def on_message(self, message: Message):
@@ -165,11 +120,11 @@ class MermaidCog(Cog, name="Mermaid", description="Generates mermaid diagrams"):
                 return
 
             ctx = await self.bot.get_context(message)
-            mermaid_script = self.parse_mermaid_code_block(message.content)
-            if not mermaid_script:
+            diagram = self.parse_code_block(message.content, mermaid_block=True)
+            if not diagram:
                 return
 
-            await ctx.reply(embed=self.create_mermaid_embed(mermaid_script))
+            await ctx.reply(embed=self.generate_diagram_embed(diagram))
 
     @Cog.listener()
     async def on_message_edit(self, before: Message, after: Message):
@@ -180,12 +135,12 @@ class MermaidCog(Cog, name="Mermaid", description="Generates mermaid diagrams"):
         :param after: Edited message
         :type after: Message
         """
-        mermaid_script = self.parse_mermaid_code_block(after.content)
-        if not mermaid_script:
+        diagram = self.parse_code_block(after.content, mermaid_block=True)
+        if not diagram:
             return
 
         ctx = await self.bot.get_context(after)
-        await ctx.reply(embed=self.create_mermaid_embed(mermaid_script))
+        await ctx.reply(embed=self.generate_diagram_embed(diagram))
 
 
 async def setup(bot):
---
+++ bot/services/mermaid_service.py
@@ -0,0 +1,61 @@
+import base64
+import requests
+
+
+MERMAID_API = "https://mermaid.ink/img/"
+
+def _encode_diagram(diagram: str) -> str:
+    """ Converts script string to base64 encoding 
+    
+    :param diagram: String to be converted
+    :type diagram: str
+
+    :returns: Converted string
+    :rtype: str
+    """
+    diagram_bytes = diagram.encode("utf8")
+    base64_bytes = base64.urlsafe_b64encode(diagram_bytes)
+    return base64_bytes.decode("ascii")
+
+def _build_url(diagram: str) -> str:
+    """ Constructs mermaid ink API url to generate mermaid diagram image
+    
+    :param diagram: Mermaid script from which the diagram will be generated
+    :type diagram: str
+
+    :returns: Mermaid ink API url
+    :rtype: str
+    """
+    encoded_diagram = _encode_diagram(diagram)
+    return f"{MERMAID_API}{encoded_diagram}"
+
+def _is_valid_diagram(url: str) -> bool:
+    """ Validates Mermaid API url
+    
+    :param url: Url to validate
+    :type url: str
+
+    :returns: Whether url is valid or not
+    :rtype: bool
+    """
+    try:
+        response = requests.get(url, timeout=5)
+        return response.status_code == 200
+    except requests.RequestException:
+        return False
+
+def generate_mermaid_diagram(diagram: str) -> str | None:
+    """ Constructs API url and validates it
+    
+    :param diagram: Mermaid diagram script
+    :type diagram: str
+
+    :returns: API url if it's valid, otherwise None
+    :rtype: str | None
+    """
+    diagram_url = _build_url(diagram)
+
+    if _is_valid_diagram(diagram_url):
+        return diagram_url
+
+    return None
