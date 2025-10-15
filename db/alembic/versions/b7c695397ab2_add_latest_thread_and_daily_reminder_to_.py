8bf83b7e6fa4e35f05f11b097ed0565353eee899
Chris <rolletchris1@gmail.com>
2025-10-15 16:37:55 -0700

    Add Alembic migration to add latest_thread (BigInteger) and daily_reminder (Boolean) columns to threads

---
+++ db/alembic/versions/b7c695397ab2_add_latest_thread_and_daily_reminder_to_.py
@@ -0,0 +1,27 @@
+"""add_latest_thread_and_daily_reminder_to_threads
+
+Revision ID: b7c695397ab2
+Revises: cc8da39749e7
+Create Date: 2025-10-15 15:58:03.467659
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "b7c695397ab2"
+down_revision = "cc8da39749e7"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.add_column("threads", sa.Column("latest_thread", sa.BigInteger(), nullable=True))
+    op.add_column("threads", sa.Column("daily_reminder", sa.Boolean()))
+
+
+def downgrade() -> None:
+    op.drop_column("threads", "latest_thread")
+    op.drop_column("threads", "daily_reminder")
