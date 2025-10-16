f05c88ce4095f8e4abdbd0ff1e9ecfb081d23114
penguinboi <simon.roy1211@gmail.com>
2025-10-16 00:48:47 -0400

    WIP fix metadata

--- db/alembic/env.py
+++ db/alembic/env.py
@@ -19,7 +19,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 # target_metadata = None
-target_metadata = app.base.metadata
+target_metadata = app.metadata
 
 
 # other values from the config, defined by the needs of env.py,
